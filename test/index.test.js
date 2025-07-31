ff76e29a8a52cae207bc91daf3be28a98cbd0631
skyhelper321 <skyhelper321@users.noreply.github.com>
2025-07-31 19:55:50 +0000

    deploy: 8922bea7060e6e3f3aa93986dc25145f55a73fd8

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,7 @@
 import { greet } from "../src/index";
 describe('greet', () => {
     it('should return greeting message', () => {
-        expect(greet("W")).toBe("Hello, W!");
+        expect(greet("Player")).toBe("Hello, Player!");
     });
 });
 //# sourceMappingURL=index.test.js.map

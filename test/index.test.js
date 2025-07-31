6eed2b7f7010637e6bf7d57937cd9dbbda2b167e
skyhelper321 <skyhelper321@users.noreply.github.com>
2025-07-31 20:07:34 +0000

    deploy: 15c717a2524ea6214fdd176e329309d39bcdd5b3

--- test/index.test.js
+++
@@ -1,7 +0,0 @@
-import { greet } from "../src/index";
-describe('greet', () => {
-    it('should return greeting message', () => {
-        expect(greet("Player")).toBe("Hello, Player!");
-    });
-});
-//# sourceMappingURL=index.test.js.map
